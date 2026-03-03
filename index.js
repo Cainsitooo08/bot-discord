4fd22db9624d149146a235de9015d749e4424d0c
Cainsitooo08 <cain08abellanas@gmail.com>
2026-03-03 20:10:27 +0100

    Refactor HTTP server to include headers and logging
    Refactor server creation to include response headers and log server start.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const http = require("http");
-http.createServer((req, res) => res.end("Bot activo")).listen(process.env.PORT || 3000);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Bot activo");
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor web iniciado");
+});
+
 const {
   Client,
   GatewayIntentBits,
